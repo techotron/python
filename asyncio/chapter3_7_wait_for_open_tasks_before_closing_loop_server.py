7f9b1194d266b679b2aabd1cf30c392df860605c
esnow <edward.snow@disneystreaming.com>
2022-09-17 14:33:54 +0100

    chapter progress

---
+++ asyncio/chapter3_7_wait_for_open_tasks_before_closing_loop_server.py
@@ -0,0 +1,77 @@
+import asyncio
+from asyncio import AbstractEventLoop
+import socket
+import logging
+import signal
+from typing import List
+ 
+ 
+async def echo(connection: socket,
+               loop: AbstractEventLoop) -> None:
+    try:
+        while data := await loop.sock_recv(connection, 1024):
+            print('got data!')
+            if data == b'boom\r\n':
+                raise Exception("Unexpected network error")
+            await loop.sock_sendall(connection, data)
+    except Exception as ex:
+        logging.exception(ex)
+    finally:
+        connection.close()
+ 
+echo_tasks = []
+ 
+ 
+async def connection_listener(server_socket, loop):
+    while True:
+        connection, address = await loop.sock_accept(server_socket)
+        connection.setblocking(False)
+        print(f"Got a connection from {address}")
+        echo_task = asyncio.create_task(echo(connection, loop))
+        echo_tasks.append(echo_task)
+ 
+ 
+class GracefulExit(SystemExit):
+    pass
+ 
+ 
+def shutdown():
+    raise GracefulExit()
+ 
+ 
+async def close_echo_tasks(echo_tasks: List[asyncio.Task]):
+    waiters = [asyncio.wait_for(task, 2) for task in echo_tasks]
+    for task in waiters:
+        try:
+            await task
+        except asyncio.exceptions.TimeoutError:
+            # We expect a timeout error here
+            pass
+ 
+ 
+async def main():
+    server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ 
+    server_address = ('127.0.0.1', 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+ 
+    # If SIGINT or SIGTERM is detected, add a sig handler function (in this case, shutdown) which will raise the GracefulExit shutdown
+    for signame in {'SIGINT', 'SIGTERM'}:
+        loop.add_signal_handler(getattr(signal, signame), shutdown)
+    await connection_listener(server_socket, loop)
+ 
+
+# We want to catch the custom exception, meaning we can't use asyncio.run() to create our loop.
+# Instead, we create our own loop and catch the GracefulExit exception 
+loop = asyncio.new_event_loop()
+
+try:
+    loop.run_until_complete(main())
+except GracefulExit:
+    # If GracefulExit exception, run all remaining tasks with a timeout
+    loop.run_until_complete(close_echo_tasks(echo_tasks))
+finally:
+    loop.close()
