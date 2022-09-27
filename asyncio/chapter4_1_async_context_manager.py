053ea465522bf269c6f6f2b3d917593436a07d43
esnow <edward.snow@disneystreaming.com>
2022-09-27 22:17:49 +0100

    chapter progress

---
+++ asyncio/chapter4_1_async_context_manager.py
@@ -0,0 +1,48 @@
+# Example of using a context manager, using the previous 
+#  chapter's example of a non blocking socket web server
+
+import asyncio
+import socket
+from types import TracebackType
+from typing import Optional, Type
+
+
+class ConnectedSocket:
+    def __init__(self, server_socket) -> None:
+        self._connection = None
+        self._server_socket = server_socket
+
+    # A special coroutine method which asynchronously acquires a resource
+    async def __aenter__(self):
+        print("Entering context manager, waiting for connection")
+        loop = asyncio.get_event_loop()
+        connection, address = await loop.sock_accept(self._server_socket)
+        self._connection = connection
+        print("Accepted a connection")
+        return self._connection
+
+    # Another special coroutine which closes the resource. The additional optional
+    #  arguments are to handle any exceptions which occur.
+    async def __aexit__(self,
+                        exc_type: Optional[Type[BaseException]],
+                        exc_val: Optional[BaseException],
+                        exc_tb: Optional[TracebackType]):
+        print("Exiting context manager")
+        self._connection.close()
+        print("Closed connection")
+
+async def main():
+    loop = asyncio.get_event_loop()
+
+    server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_address = ("127.0.0.1", 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+
+    async with ConnectedSocket(server_socket) as connection:
+        data = await loop.sock_recv(connection, 1024)
+        print(data)
+
+asyncio.run(main())
