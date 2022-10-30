396d8ee2fb1d32a6ff24eb281b0e8decf07c5113
esnow <edward.snow@disneystreaming.com>
2022-10-30 19:18:08 +0000

    add queue example

--- asyncio/queue_example.py
+++ asyncio/queue_example.py
@@ -1,9 +1,8 @@
 import asyncio
 from time import sleep
 
-concurrency = 5
-total = 20
-
+concurrency = 25
+total = 200
 
 class My_Async_Queue():
     def __init__(self) -> None:
@@ -12,7 +11,9 @@ class My_Async_Queue():
         self.third_q = asyncio.Queue()
 
     async def first_delay(self):
+        print("starting first delay loop")
         while True:
+            # Await here blocks the coroutine until something is returned from the queue.get() call
             i = await self.first_q.get()
             print(f"[first] sleeping start - {i}")
             await asyncio.sleep(1)
@@ -21,7 +22,9 @@ class My_Async_Queue():
             self.first_q.task_done()
 
     async def second_delay(self):
+        print("starting second delay loop")
         while True:
+            # Await here blocks the coroutine until something is returned from the queue.get() call
             i = await self.second_q.get()
             print(f"[second] sleeping start - {i}")
             await asyncio.sleep(1)
@@ -30,7 +33,9 @@ class My_Async_Queue():
             self.second_q.task_done()
 
     async def third_delay(self):
+        print("starting third delay loop")
         while True:
+            # Await here blocks the coroutine until something is returned from the queue.get() call
             i = await self.third_q.get()
             print(f"[third] sleeping start - {i}")
             await asyncio.sleep(1)
@@ -40,7 +45,7 @@ class My_Async_Queue():
 
     async def run(self):
         # Multi-dimensional array of tasks. This will spawn sets of $concurrency number of tasks, acting as workers.
-        #  They do no run until we hit (almost) any "await" keyword
+        #  In our case here, the tasks are forever loops which are await-ing on a queue.get() call to return.
         workers = [
             [
                 asyncio.create_task(self.first_delay()) for _ in range(concurrency)
@@ -53,37 +58,23 @@ class My_Async_Queue():
             ]
         ]
 
-        # At this point, nothing is executed yet. Tasks are queued but not actually running
+        # At this point, we have a number (concurrency * len(workers)) of coroutines which are all blocked, waiting on their respective queues to have items on them.
+
 
         for i in range(total):
-            # TODO: For some reason, even though we have tasks scheduled at this point, they're not triggered by this await keyword...
-            await self.first_q.put(i) 
-
-        # At this point, still nothing is being executed. Items have been added to the first queue though
-
-        # If the below 2 lines were uncommented, the worker tasks would be triggered and start executing ALL items in "total". The only
-        #  thing the sleep(10000) is blocking are the tasks getting cancelled, so the script would just hang for 10000 seconds.
-        # await asyncio.sleep(i)
-        # sleep(10000)
-
+            # This is where we first unblock the queues by adding an item onto the first one. 
+            await self.first_q.put(i)
 
         for q in [self.first_q, self.second_q, self.third_q]:
-            # NOW things are getting executed. First queue consists of 20 items. The following is from the first of a "print(q)":
-            #   <Queue maxsize=0 _queue=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19] tasks=20>
-
-            # join() will block until the queues have been processed, so this just ensures that all the queues are empty before cancelling the tasks
-            #  The functions themselves are passing "items" down the chain (first_delay -> second_delay -> third_delay)
-
-            # This await triggers the tasks defined in workers. So we'd see concurrency * len(workers) tasks. Or more precisely, tasks for:
-            #   concurrency * first_delay()
-            #   concurrnecy * second_delay()
-            #   concurrency * third_delay()
-            # Each of them are a consumer/producer which are poping items off the queue and putting them onto the next queue in the chain.
+            # queue.join() will block execution (ie, cancelling the workers) until the queues have been fully processed, so this just ensures 
+            #  that all the queues are empty before cancelling the tasks.
+            # The functions themselves are passing "items" down the chain (first_delay -> second_delay -> third_delay)
             await q.join()
 
         for ws in workers:
             for w in ws:
                 w.cancel()
+
 
 
 queue = My_Async_Queue()
